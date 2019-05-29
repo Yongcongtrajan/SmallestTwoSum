0973d55167b437c7a243150410bce67b7345e926
ryan_global <yongcongzou@gmail.com>
2019-05-29 18:37:52 -0500

    updated version 3.0 from second branch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main (String [] args) {
         System.out.println(Solution.minSum(new int [] {2006,1,50}));
-        System.out.println("this is version 2.0 ");
+        System.out.println("this is version 3.0 ");
 
 
     }
