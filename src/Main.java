21b0496e0c5b9f8786a1946a7a82d2132b7df2d9
ryan_global <yongcongzou@gmail.com>
2019-05-29 22:26:13 -0500

    xiaojiajia

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main (String [] args) {
         System.out.println(Solution.minSum(new int [] {2006,1,50}));
-
-        System.out.println("this is version 5.0 from devil and xiaojiajia branch ");
-
-
-
+        System.out.println("this is version 3" +
+                ".0 ");
+        System.out.println("bug fixed ");
 
 
     }
