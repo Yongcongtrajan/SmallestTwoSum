04ae0dc5fc03ce4931aa84eb8bf8f4061681df7d
ryan_global <yongcongzou@gmail.com>
2019-05-29 21:59:39 -0500

    merge between master and second branch
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main (String [] args) {
         System.out.println(Solution.minSum(new int [] {2006,1,50}));
 
-        System.out.println("this is version 5.0 from master and second branch ");
+        System.out.println("this is version 5.0 from devil and xiaojiajia branch ");
 
 
 
